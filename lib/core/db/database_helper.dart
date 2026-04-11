2dd680124d1d333d413a90fef41d5174f6908b11
Abdullah KINIK <akinik456@gmail.com>
2026-04-11 03:00:45 +0300

    feat: add sqlite database and vault table

---
+++ lib/core/db/database_helper.dart
@@ -0,0 +1,38 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('lynra.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+CREATE TABLE vault (
+  id TEXT PRIMARY KEY,
+  payload TEXT NOT NULL,
+  createdAt INTEGER NOT NULL,
+  updatedAt INTEGER NOT NULL,
+  isFavorite INTEGER NOT NULL
+)
+''');
+  }
+}
